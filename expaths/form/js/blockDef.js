b884f52ba0ba669cdcf4ab1f2affbf2a6440952b
Alexandru Dobre <alexandru.dobre@webtales.fr>
2016-06-15 10:58:42 +0200

    add angular service

--- expaths/form/js/blockDef.js
+++ expaths/form/js/blockDef.js
@@ -3,3 +3,19 @@ blocksConfig.carrouselVideo={
     "externalDependencies":["/extension-path/form/src/modules/rubedoBlocks/controllers/CarrouselVideoController.js",'/components/OwlFonk/OwlCarousel/owl-carousel/owl.carousel.min.js'],
     "absoluteUrl": true
 };
+angular.module("rubedoDataAccess").factory('Journaux',['$http',function($http){
+    var serviceInstance = {};
+    serviceInstance.getList=function(){
+        return ($http.get("/api/v1/journaux"));
+    };
+    serviceInstance.addJournal=function(titre){
+        return ($http({
+            url:"/api/v1/journaux",
+            method:"POST",
+            data:{
+                titre:titre
+            }
+        }));
+    };
+    return serviceInstance;
+}]);
